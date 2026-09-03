fff5123583d1af37d8267fb9d95b6f6dd14bc894
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-03 11:59:58 +0200

    Byte slices spell string literals; [Byte](utf8:)

--- Sources/Byte Standard Library Integration/Swift.Array+Byte.swift
+++ Sources/Byte Standard Library Integration/Swift.Array+Byte.swift
@@ -26,3 +26,38 @@ where Element == Byte {
         self.init(stringLiteral: value)
     }
 }
+
+extension Swift.Array where Element == Byte {
+
+    @inlinable
+    public init(utf8 string: String) {
+        self = string.utf8.map(Byte.init(bitPattern:))
+    }
+}
+
+extension Swift.ArraySlice: @retroactive ExpressibleByUnicodeScalarLiteral,
+    @retroactive ExpressibleByExtendedGraphemeClusterLiteral,
+    @retroactive ExpressibleByStringLiteral
+where Element == Byte {
+
+    public typealias StringLiteralType = String
+
+    public typealias ExtendedGraphemeClusterLiteralType = String
+
+    public typealias UnicodeScalarLiteralType = String
+
+    @inlinable
+    public init(stringLiteral value: String) {
+        self = [Byte](stringLiteral: value)[...]
+    }
+
+    @inlinable
+    public init(extendedGraphemeClusterLiteral value: String) {
+        self.init(stringLiteral: value)
+    }
+
+    @inlinable
+    public init(unicodeScalarLiteral value: String) {
+        self.init(stringLiteral: value)
+    }
+}
